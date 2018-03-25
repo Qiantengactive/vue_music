95a9f9a065f437d3864eb681c38c652cfc949eb2
qt-laptop <936129056@qq.com>
2018-03-25 23:57:01 +0800

    add getlistDic

--- src/api/config.js
+++ src/api/config.js
@@ -2,10 +2,13 @@ export const commonParams = {
   g_tk: 5381,
   inCharset: 'utf-8',
   outCharset: 'utf-8',
-  notice: 1,
+  notice: 0,
   format: 'jsonp'
 }
+
 export const options = {
-  param: 'jsonpCallback'
+  param: 'jsonpCallback',
+  prefix: 'jp'
 }
+
 export const ERR_OK = 0
