5607af53a7a988ebc66e2ac0402560fcc7cbc028
huolinag <936129056@qq.com>
2018-04-02 20:31:06 +0800

    fix api

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="singer" ref="singer">
+    <v-listview @select="selectSinger" :data="singers" ref="list"></v-listview>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+import listView from 'base/listview/listview'
+import { getSingerList } from 'api/singer'
+import ERR_OK from '@/api/config'
+export default {
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    handlePlaylist (playlist) {
+
+    },
+    selectSinger (singer) {
+
+    },
+    _getSingerList () {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+
+        }
+      })
+    },
+    _normalizeSinger (list) {
+
+    }
+  },
+  components: {
+    'v-listview': listView
+  }
+}
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+.singer {
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
+}
+</style>
